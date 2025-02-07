063a3cfa6b33156a73b6e887c567c5044db9ee0b
Victor Arantes <victorarantes000@gmail.com>
2025-02-07 08:58:41 -0300

    Teste api token

--- src/main/java/com/template/core/config/SecurityConfig.java
+++ src/main/java/com/template/core/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.template.core.config;
 
 import com.template.core.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Base64;
 
 /**
  * Esta classe configura a segurança do Spring Security na aplicação.
@@ -25,6 +30,18 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final TokenService tokenService;
     private final UserService userService;
+
+    @Value("${api.key}")
+    public String apiKey;
+
+    private boolean validateAPIKey(String key) {
+        try {
+            String decodedKey = new String(Base64.getDecoder().decode(key.getBytes()));
+            return decodedKey.equals(apiKey);
+        } catch (Exception e) {
+            throw new AccessDeniedException(e.getMessage());
+        }
+    }
 
     /**
      * Configura o filtro de segurança para as requisições HTTP.
@@ -37,7 +54,14 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/swagger-ui.html/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().access((authenticationSupplier, request) -> {
+                            String tempApiKey = request.getRequest().getHeader("X-Service-Token");
+
+                            if (validateAPIKey(tempApiKey)) {
+                                return new AuthorizationDecision(true);
+                            }
+                            throw new AccessDeniedException("Access Denied");
+                        }))
                 .addFilterBefore(new AuthenticationFilter(tokenService, userService), UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
